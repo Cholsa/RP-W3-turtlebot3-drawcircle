f46d9470a45ad9cca6b97bf5ed4663331fb8596a
Seawater007 <cholsaseawater@gmail.com>
2024-04-05 21:43:09 +0700

    create pkg and scripts

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -6,5 +6,7 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("my_circle_package")
+subdirs("my_star_package")
 subdirs("turtlebot3_simulations/turtlebot3_fake")
 subdirs("turtlebot3_simulations/turtlebot3_gazebo")
