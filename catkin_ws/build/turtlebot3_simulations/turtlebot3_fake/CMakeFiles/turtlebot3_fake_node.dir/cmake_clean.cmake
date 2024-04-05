d7f6749a1a9e084acaf4b2e9fba7f41429b752b3
Seawater007 <cholsaseawater@gmail.com>
2024-04-05 21:10:38 +0700

    creating the workspace and clone

---
+++ catkin_ws/build/turtlebot3_simulations/turtlebot3_fake/CMakeFiles/turtlebot3_fake_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu20-04/assignments/RP-W3-turtlebot3-drawcircle/catkin_ws/devel/lib/turtlebot3_fake/turtlebot3_fake_node"
+  "/home/ubuntu20-04/assignments/RP-W3-turtlebot3-drawcircle/catkin_ws/devel/lib/turtlebot3_fake/turtlebot3_fake_node.pdb"
+  "CMakeFiles/turtlebot3_fake_node.dir/src/turtlebot3_fake.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtlebot3_fake_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
